0465dbea8024e26c095cba45ff24c3e49ff41d88
Mendel Hornbacher <me@mendel.tech>
2017-06-04 11:31:37 -0400

    Jobs migrations

--- db/migrate/20170604152256_create_jobs.rb
+++ db/migrate/20170604152256_create_jobs.rb
@@ -1,4 +1,9 @@
 class CreateJobs < ActiveRecord::Migration[5.1]
   def change
+    create_table :jobs do |t|
+      t.integer :client_id
+      t.datetime  :date
+      t.float :amount_paid
+    end
   end
 end
--- db/migrate/20170604152353_create_job_services.rb
+++ db/migrate/20170604152353_create_job_services.rb
@@ -1,4 +1,8 @@
 class CreateJobServices < ActiveRecord::Migration[5.1]
   def change
+    create_table :job_services do |t|
+      t.integer :job_id
+      t.integer :service_id
+    end
   end
 end
