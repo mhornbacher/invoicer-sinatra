9b5113e1a993d56312db1f81350c1025b267c259
Mendel Hornbacher <me@mendel.tech>
2017-06-04 11:27:42 -0400

    service inital migration

--- db/migrate/20170604152246_create_services.rb
+++ db/migrate/20170604152246_create_services.rb
@@ -1,4 +1,9 @@
 class CreateServices < ActiveRecord::Migration[5.1]
   def change
+    create_table :services do |t|
+      t.string  :name
+      t.float   :price
+      t.integer :user_id
+    end
   end
 end
