2e4602368d1bb48a0bda78a468ebf63738ca5e15
Mendel Hornbacher <me@mendel.tech>
2017-06-04 11:46:11 -0400

    added validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
     has_secure_password
+
+    # Relationships
     has_many :clients
     has_many :jobs, through: :clients
     has_many :services
+
+    # Validation
+    validates :username, :email, presence: true
+    validates_uniqueness_of :username   # Only one user can have this username
 end
