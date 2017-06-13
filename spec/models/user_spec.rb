19edae7bf71995ed0ae876c788dd10635d4b2591
Mendel Hornbacher <me@mendel.tech>
2017-06-12 22:39:51 -0400

    added basic user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,25 @@
 require 'spec_helper'
 
 describe User do
-    
-    it 'has a username and validates that it exists' do
+
+    it 'has a username, email and password and validates that they exists' do
+        expect(User.new).to_not be_valid
+        expect(User.new(username: "test")).to_not be_valid
+        expect(User.new(username: "test", password: "test")).to_not be_valid
+        expect(User.new(password: "test", email: "tmp")).to_not be_valid
+        expect(User.new(username: "test", password: "test", email: "test@test.com")).to be_valid
     end
 
     it 'validates that that username is unique' do
-    end
-    
-    it 'has a email and validates that it exists' do
-    end
-    
-    it 'has a password and validates that it exists' do
+        user_hash = {username: "duplicate", email: "test@test.com", password: "tmp"}
+        user1 = User.create(user_hash)
+        expect(User.new(user_hash)).to_not be_valid
     end
 
     it 'has secure password' do
+        user = User.new(password: "test")
+        expect(user.authenticate("not_good")).to eq(false)
+        expect(user.authenticate("test")).to eq(user)
     end
     
     it 'has many clients' do
