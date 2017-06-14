32dfecf86631d795e65e8aa6db86bcbdec8b8202
Mendel Hornbacher <me@mendel.tech>
2017-06-14 17:24:20 -0400

    fixed long trailing numbers

--- app/helpers/numberFormat.rb
+++ app/helpers/numberFormat.rb
@@ -1,6 +1,7 @@
 module NumberFormatter
     def number_to_currency(number)
         # Formatter taken from https://stackoverflow.com/questions/4175326/does-ruby-have-any-number-formatting-classes
-        number.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+        number = number.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse.split(".")
+        "#{number[0]}.#{number[1][0..1]}"
     end
 end
